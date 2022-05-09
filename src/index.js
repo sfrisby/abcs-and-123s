0d9f71337cb4db66f79bc84f714aea841d57e2a1
Spencer Frisby <frisbysg@gmail.com>
2022-05-09 09:21:11 -0600

    styling

--- src/index.js
+++ src/index.js
@@ -11,9 +11,15 @@ $(document).ready(function () {
     setRandomLinearBackground('body');
 
     // document.getElementById("navbarAboutBtn").click();
-    document.getElementById("navbarPreKNumberBtn").click();
-    //document.getElementById("numbersOrderedBtn").click();
-    document.getElementById("numberQuizBtn").click();
+    // document.getElementById("navbarPreKNumberBtn").click();
+    // document.getElementById("numberOrderedBtn").click();
+    // document.getElementById("numberQuizBtn").click();
+    // document.getElementById("numberRandBtn").click();
+    
+    document.getElementById("navbarPreKLetterBtn").click();
+    // document.getElementById("letterQuizBtn").click();
+    document.getElementById("letterAlphabetBtn").click();
+    
 
     setOrderedNumbers();
     setRandomNumber(false);
